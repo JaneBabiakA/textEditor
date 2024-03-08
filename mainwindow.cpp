ec43df8f64ad0a06e0c315e8d35e00782a6b889f
Jane Babiak-Abray <janefrances369@gmail.com>
2024-03-07 22:14:41 -0500

    Added basic file handling and save button

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,24 @@
 #include "mainwindow.h"
 #include <QPushButton>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QFileDialog>
 
 mainWindow::mainWindow(QWidget *parent) :
     QWidget(parent)
 {
+    QHBoxLayout *button_box = new QHBoxLayout(this);
+    //QLineEdit *doc_box = new QLineEdit(this);
     QPushButton *new_button = new QPushButton("New", this);
-    //QPushButton *open_button = new QPushButton("Open", this);
+    QPushButton *open_button = new QPushButton("Open", this);
+    QPushButton *save_button = new QPushButton("Save", this);
+    button_box->addWidget(new_button);
+    button_box->addWidget(open_button);
+    button_box->addWidget(save_button);
     connect(new_button, SIGNAL (clicked()), this, SLOT (createDocument()));
+    connect(open_button, SIGNAL (clicked()), this, SLOT (openDocument()));
+    connect(save_button, SIGNAL (clicked()), this, SLOT (saveDocument()));
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,8 +3,14 @@
 
 #include <QWidget>
 #include "document.h"
+#include <QFileDialog>
+#include <fstream>
 
 class QPushButton;
+class QHBoxLayout;
+class QVBoxLayout;
+class QLineEdit;
+class QFileDialog;
 class Document;
 class mainWindow : public QWidget
 {
@@ -14,12 +20,31 @@ public:
 
 private slots:
     void createDocument(){
-        QTextStream(stdout) << "button pressed";
+        QTextStream(stdout) << "new";
+        // should save current document & then clear textbox
+        return;
+    }
+    void openDocument(){
+        std::string line;
+        std::string fileName = QFileDialog::getOpenFileName(this, "", "", "Text files (*.txt)").toStdString();
+        std::ifstream file(fileName);
+        QTextStream out(stdout);
+        while(std::getline(file, line)){
+            out << QString::fromStdString(line);
+        }
+        return;
+    }
+    void saveDocument(){
+        //saves document
         return;
     }
 private:
+    QVBoxLayout *window_box;
+    QHBoxLayout *button_box;
     QPushButton *new_button;
     QPushButton *open_button;
+    QPushButton *save_button;
+    QLineEdit *doc_box;
 };
 #endif // MAINWINDOW_H
 
